a6d64d9d78373ee0d44c1a85a93f84d9e135a7be
Nabila Caesarani Putri Alamsyah <34364931+nabilacsrn@users.noreply.github.com>
2019-04-06 11:27:28 +0700

    Update No1.java

--- No1/src/no1/No1.java
+++ No1/src/no1/No1.java
@@ -3,23 +3,29 @@ package no1;
 import java.util.Scanner;
 
 public class No1 {
+    static int calculateSum(int n) 
+    { 
+        if (n <= 0) 
+           return 0; 
+       
+        int fibo[]=new int[n+1]; 
+        fibo[0] = 0; fibo[1] = 1; 
+       
+        int sum = fibo[0] + fibo[1]; 
+       
+        for (int i=2; i<=n; i++) 
+        { 
+            fibo[i] = fibo[i-1]+fibo[i-2]; 
+            sum += fibo[i]; 
+        } 
+       
+        return sum; 
+    }
+    
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int i = 1, t1 = 1, t2 = 1;
+        Scanner show = new Scanner(System.in);
         System.out.print("Input : ");
-        int in = scan.nextInt();
-        
-        System.out.println("Output : ");
-
-        while (i <= in)
-        {
-            System.out.print(t1 + " ");
-
-            int sum = t1 + t2;
-            t1 = t2;
-            t2 = sum;
-
-            i++;
-        }
+        int n = show.nextInt();
+        System.out.println("Output : \n"+ calculateSum(n)); 
     }
 }
